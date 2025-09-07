7ae2b27364b69ed1189252f1726ca75d8847df7d
Myslitel666 <tretyakov_02@bk.ru>
2025-09-07 15:41:39 +0500

    first commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,56 @@
+<script>
+  import ColorThemeSwitch from "svelte-elegant/ColorThemeSwitch";
+  import { goto } from "$app/navigation";
+  import Header from "svelte-elegant/Header";
+  import { Calculator } from "svelte-elegant/icons-elegant";
+
+  import { themeMode, themeStore } from "svelte-elegant/stores";
+
+  let theme;
+
+  let logotypeFilter = "";
+  let svelteColor = "";
+  let elegantColor = "";
+
+  // Подписываемся на изменения темы
+  themeStore.subscribe((value) => {
+    theme = value; //Инициализация объекта темы
+
+    svelteColor = theme.palette.primary;
+    elegantColor = $themeMode === "light" ? "#383838" : "#fdfdfd";
+    logotypeFilter = $themeMode === "light" ? "brightness(80%)" : "";
+  });
+</script>
+
+<Header>
+  <button
+    style:margin-left="-2px"
+    style:gap="1px"
+    onclick={() => goto("/settings")}
+  >
+    <Calculator size="38px" />
+    <p style:font-size="26px">
+      <span
+        style:color={svelteColor}
+        style:filter={logotypeFilter}
+        style:transition="all 0.3s"
+      >
+        Calculator
+      </span>
+    </p>
+  </button>
+  <div style:margin-left="auto" style:margin-right="1px">
+    <ColorThemeSwitch />
+  </div>
+</Header>
+
+<style>
+  button {
+    margin-left: 6px;
+  }
+  @media (max-width: 768px) {
+    button {
+      margin-left: 3px;
+    }
+  }
+</style>
